1d2f136640c3379dea69ef5b3ecfc7899180c459
Z2gK <muscadomestica@hotmail.com>
2020-07-08 23:33:30 +0800

    small tweaks to evtlog

--- evtlog/evtxeventid.py
+++ evtlog/evtxeventid.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul  6 12:01:18 2020
+# a script to help construct dictionaries of interesting event IDs from a security viewpoint
+# 'Interesting' IDs are taken from open sources referenced here
+# Some initial text processing is required
 
-"""
 # extracted from
 # https://www.exabeam.com/siem-guide/siem-concepts/event-log/
 securityevts1 = {4624: 'Successful log on', 4625: 'Failed log on', 4634: 'Account log off', 4648: 'Log on attempt with explicit credentials', 4719: 'System audit policy change', 4964: 'Special group assigned to new log on attempt', 1102: 'Audit log cleared', 4720: 'New user account created', 4722: 'User account enabled', 4723: 'Attempt to change password', 4725: 'User account disabled', 4728: 'User added to privileged global group', 4732: 'User added to privileged local group', 4756: 'User was added to privileged universal group', 4738: 'Change to user account', 4740: 'User locked out of an account', 4767: 'User account unlocked', 4735: 'Change to privileged local group', 4737: 'Change to privileged global group', 4755: 'Change to universal group', 4772: 'Failed request for Kerberos ticket', 4777: 'Domain controller failed to validate credentials', 4782: 'Account password hash accessed', 4616: 'System time changed', 4657: 'Change to registry value', 4697: 'Service install attempt', 4946: 'Rule added to Windows Firewall exception', 4947: 'Rule modified in Windows Firewall exception', 4950: 'Windows Firewall settings change', 4954: 'Change to Windows Firewall Group Policy'}
@@ -96,7 +95,7 @@ for k in fwevts2:
 print(len(fwevts))
 print(fwevts)
 
-# Event ID output - consolidated list to use for filters
+# Event ID output - consolidated list to use for filters, for a start
 # {4624: 'Successful log on', 4625: 'Failed log on', 4634: 'Account log off', 4648: 'Log on attempt with explicit credentials', 4719: 'System audit policy change', 4964: 'Special group assigned to new log on attempt', 1102: 'Audit log cleared', 4720: 'New user account created', 4722: 'User account enabled', 4723: 'Attempt to change password', 4725: 'User account disabled', 4728: 'User added to privileged global group', 4732: 'User added to privileged local group', 4756: 'User was added to privileged universal group', 4738: 'Change to user account', 4740: 'User locked out of an account', 4767: 'User account unlocked', 4735: 'Change to privileged local group', 4737: 'Change to privileged global group', 4755: 'Change to universal group', 4772: 'Failed request for Kerberos ticket', 4777: 'Domain controller failed to validate credentials', 4782: 'Account password hash accessed', 4616: 'System time changed', 4657: 'Change to registry value', 4697: 'Service install attempt', 4946: 'Rule added to Windows Firewall exception', 4947: 'Rule modified in Windows Firewall exception', 4950: 'Windows Firewall settings change', 4954: 'Change to Windows Firewall Group Policy', 4724: 'An attempt was made to reset an accounts password', 4704: 'A user right was assigned', 4717: 'System security access was granted to an account', 4739: 'Domain Policy was changed', 4698: 'A scheduled task was created', 4699: 'A scheduled task was deleted', 4700: 'A scheduled task was enabled', 4701: 'A scheduled task was disabled', 4702: 'A scheduled task was updated'}
 # {5025: 'Windows Firewall service stopped', 5031: 'Application blocked by Windows Firewall from accepting traffic', 5155: 'Windows Filtering Platform blocked a service from listening on a port', 5152: 'The Windows Filtering Platform blocked a packet', 5153: 'A more restrictive Windows Filtering Platform filter has blocked a packet', 5157: 'Windows Filtering Platform blocked a connection', 5447: 'A Windows Filtering Platform filter was changed'}
 
